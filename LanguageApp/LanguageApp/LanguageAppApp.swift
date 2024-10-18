ca269444317893e8e96013c9c24a3cb7db6153e0
maddiehutch1 <madhutch@byu.edu>
2024-10-17 22:48:15 -0600

    Initial push of changes to github

---
+++ LanguageApp/LanguageApp/LanguageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageAppApp.swift
+//  LanguageApp
+//
+//  Created by IS 543 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LanguageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
